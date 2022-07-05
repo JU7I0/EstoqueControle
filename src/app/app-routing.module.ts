e75d3fda3a1b5afcff79c0de170c3b5021d17215
Julio Barros <juliocezarbarros2@gmail.com>
2022-07-05 00:16:13 -0300

    Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', redirectTo:'cadastro', pathMatch:'full'},
+  { path:'cadastro', component:CadastroComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
