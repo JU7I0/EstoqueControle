03efd9dd85617dcef032959226d873ff7a30b387
Julio Barros <juliocezarbarros2@gmail.com>
2022-07-05 12:06:08 -0300

    Rotas para path  /dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { EntrarComponent } from './entrar/entrar.component';
 
 const routes: Routes = [
   { path:'', redirectTo:'entrar', pathMatch:'full'},
   { path:'cadastro', component:CadastroComponent },
-  { path:'entrar', component:EntrarComponent}
+  { path:'entrar', component:EntrarComponent},
+  { path:'dashboard', component:DashboardComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { EntrarComponent } from './entrar/entrar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CadastroComponent,
-    EntrarComponent
+    EntrarComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
